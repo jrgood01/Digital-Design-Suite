1c43b8086232ab8fa0ed15cf68adc723e4b13345
jhaygood01 <jake.haygood01@gmail.com>
2022-01-18 12:35:58 -0500

    Init Commit

---
+++ Digital-Design-Suite/src/app.tsx
@@ -0,0 +1,21 @@
+//Software licensed under creative commons Attribution-NonCommercial-NoDerivatives 4.0
+//
+//  You may not re-dsitrubte this software with modification or use this software 
+//  for commercial purposes without written permission from the owner
+//
+//Copyright Jacob R. Haygood 2022
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './default.css';
+
+function render() {
+  ReactDOM.render(
+    <div style={{width:"100vw", height: "100vh", background:"#141216"}}>
+
+    </div>
+  , document.body
+  );
+}
+
+render();
