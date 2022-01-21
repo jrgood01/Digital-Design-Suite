72bae43fb9ff80cfb1e4261380d3e543be87f15d
jhaygood01 <jake.haygood01@gmail.com>
2022-01-21 11:44:51 -0500

    Finsihed logic gate components

---
+++ Digital-Design-Suite/src/SimulationCore/SimulationComponent/Gates/XORGate.ts
@@ -0,0 +1,34 @@
+//Software licensed under creative commons Attribution-NonCommercial-NoDerivatives 4.0
+//
+//  You may not re-dsitrubte this software with modification or use this software 
+//  for commercial purposes without written permission from the owner
+//
+//Copyright Jacob R. Haygood 2022
+
+class ORGate extends SimulationComponent {
+    inputs : number;
+    bitWidth : number;
+
+    constructor(bitWidth : number, numInputs : number) {
+        super(numInputs, 1, Array(bitWidth).fill(numInputs), Array(bitWidth).fill(1));
+        this.inputs = numInputs;
+        this.bitWidth = bitWidth;
+    }
+
+    simulate() {
+        for (let bit = 0; bit < this.bitWidth; bit ++) {
+            let outputBit = false;
+            for (let line = 0; line < this.inputs; line ++) {
+                if (this.input.getLineBit(line, bit) == true) {
+                    if (outputBit == true) {
+                        this.output.setLineBit(0, bit, outputBit);
+                        return;
+                    } else {
+                        outputBit = true;
+                    }
+                }
+            }
+            this.output.setLineBit(0, bit, outputBit);
+        }
+    }
+}
