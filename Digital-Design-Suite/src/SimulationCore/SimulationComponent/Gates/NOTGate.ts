d1504f8f963b5ec434764cf788be8867cd148016
jhaygood01 <jake.haygood01@gmail.com>
2022-01-21 12:05:09 -0500

    Added not gate

---
+++ Digital-Design-Suite/src/SimulationCore/SimulationComponent/Gates/NOTGate.ts
@@ -0,0 +1,24 @@
+//Software licensed under creative commons Attribution-NonCommercial-NoDerivatives 4.0
+//
+//  You may not re-dsitrubte this software with modification or use this software 
+//  for commercial purposes without written permission from the owner
+//
+//Copyright Jacob R. Haygood 2022
+
+class NOTGate extends SimulationComponent {
+    inputs : number;
+    bitWidth : number;
+
+    constructor(bitWidth : number, numInputs : number) {
+        super(1, 1, Array(bitWidth).fill(1), Array(bitWidth).fill(1));
+        this.inputs = numInputs;
+        this.bitWidth = bitWidth;
+    }
+
+    simulate() {
+        for (let bit = 0; bit < this.bitWidth; bit ++) {
+            let outputBit = !this.input.getLineBit(0, bit);
+            this.output.setLineBit(0, bit, outputBit);
+        }
+    }
+}
