6285ae87c9908a5cabbf10ebd9eeb11630633559
jhaygood01 <jake.haygood01@gmail.com>
2022-01-24 17:00:19 -0500

    Increase code quality of SimulationComponentIO

--- Digital-Design-Suite/src/SimulationCore/Simulation.ts
+++ Digital-Design-Suite/src/SimulationCore/Simulation.ts
@@ -19,28 +19,43 @@ export class DigitalDesignSimulation extends PIXI.Application{
 
     ticker : PIXI.Ticker;
     interactionManager : InteractionManager;
-
+    grid : PIXI.Graphics;
     constructor(container : HTMLDivElement) {
         super();
         
+        //Bind methods to class
         this.resizePixiSimulation = this.resizePixiSimulation.bind(this);
         this.onMouseDown = this.onMouseDown.bind(this);
         this.onMouseUp = this.onMouseUp.bind(this);
         this.onMouseMove = this.onMouseMove.bind(this);
-        this.onScroll = this.onScroll.bind(this)
-
+        this.onScroll = this.onScroll.bind(this);
         this.addComponent = this.addComponent.bind(this);
         this.simulationLoop = this.simulationLoop.bind(this);
+
+        //Simulation state holds data about the simulation
         this.simulationState = new SimulationState();
+        //We'll use the pixi.js interaction manager to determine if
+        //  clicks and other actions are inside a component.
         this.interactionManager = new InteractionManager(this.renderer);
+
+        //Set the pixi.js view to the size of the window
         this.view.width = window.innerWidth;
         this.view.height = window.innerHeight;
 
+        //Add the view to the HTML body
         document.body.append(this.view);
 
+        //Add a new ticker to the simulation. The ticker will
+        //  run on a set interval and render simulation graphics
         this.ticker = PIXI.Ticker.shared.add(this.simulationLoop, this)
+        this.ticker.maxFPS = 10;
+
+        //This method resizes the pixi.js render area to the
+        //  size of the window
         this.resizePixiSimulation();
         
+        //We want to resize the view every time the window is
+        //  resized
         window.addEventListener('resize', this.resizePixiSimulation);  
 
         this.stage.interactive = true;
@@ -50,23 +65,26 @@ export class DigitalDesignSimulation extends PIXI.Application{
         this.simulationState.addComponent(v)
         this.simulationState.addComponent(v2)
         this.renderer.backgroundColor = constants.General.BgColor_1
-       
+        
+        //Add listeners for global events
         document.addEventListener("wheel", this.onScroll)
         document.addEventListener("mousedown", this.onMouseDown);
         document.addEventListener("mouseup", this.onMouseUp);
-        document.addEventListener("mousemove", this.onMouseMove);
+        document.addEventListener("mousemove", this.onMouseMove);  
     }
 
     addComponent(component : SimulationComponent) {
         this.simulationState.addComponent(component);
     }
 
-
+    //Renders the simulation graphics on a set interval
     simulationLoop(delta : number){
-        this.simulationState.components.forEach((c) => {
-            c.draw();
-        })
-
+        if (this.simulationState.stateChanged) {
+            this.simulationState.components.forEach((c) => {
+                c.draw();
+            })
+            this.simulationState.stateChanged = false;
+         }
     }
 
     resizePixiSimulation() {
@@ -76,7 +94,7 @@ export class DigitalDesignSimulation extends PIXI.Application{
 
     onScroll(scrollEvent : WheelEvent) {
         if (this.isInside(scrollEvent.clientX, scrollEvent.clientY, 
-            this.view.getBoundingClientRect()) && this.simulationState.getMode() == MouseMode.ZOOM) {
+            this.view.getBoundingClientRect())) {
                 this.stage.scale.x -= scrollEvent.deltaY / 1000;
                 this.stage.scale.y -= scrollEvent.deltaY / 1000;
         }
@@ -91,20 +109,26 @@ export class DigitalDesignSimulation extends PIXI.Application{
     }
 
     onMouseDown(mouseEvent : MouseEvent) {
-        if (this.isInside(mouseEvent.clientX, mouseEvent.clientY, this.view.getBoundingClientRect())) {
-            this.simulationState.isPanning = true;
-        }
 
         let hit = this.interactionManager.hitTest(new PIXI.Point(mouseEvent.x, mouseEvent.y), this.stage)
 
         this.simulationState.components.forEach((component : SimulationComponent) => {
             if (component.componentTemplate == hit) {
                 component.selected = true;
+                this.simulationState.isDraggingComponent = true;
+                this.simulationState.stateChanged = true;
+                this.simulationState.SelectedComponent = component;
+                this.simulationState.stateChanged = true;
             } else {
                 component.selected = false;
+                this.simulationState.stateChanged = true;
             }
         })
 
+        if (!this.simulationState.isDraggingComponent && this.isInside(mouseEvent.clientX, mouseEvent.clientY, this.view.getBoundingClientRect())) {
+            this.simulationState.isPanning = true;
+        }
+        this.simulationState.stateChanged = true;
     }
 
     onMouseUp(mouseEvent : MouseEvent) {
@@ -112,14 +136,23 @@ export class DigitalDesignSimulation extends PIXI.Application{
             this.simulationState.isPanning) {
                 this.simulationState.isPanning = false;
             }
+
+            this.simulationState.isDraggingComponent = false;
     }
 
     onMouseMove(mouseEvent : MouseEvent) {
         if (this.simulationState.isPanning) {
-            this.stage.pivot.x -= mouseEvent.movementX;
-            this.stage.pivot.y -= mouseEvent.movementY;
+            //this.stage.pivot.x -= mouseEvent.movementX;
+            //this.stage.pivot.y -= mouseEvent.movementY;
+        }
+
+        if (this.simulationState.isDraggingComponent) {
+            this.simulationState.SelectedComponent.x += mouseEvent.movementX;
+            this.simulationState.SelectedComponent.y += mouseEvent.movementY;
+            this.simulationState.stateChanged = true;
         }
     }
+
     isInside(x : number, y : number, rect : DOMRect) {
         return (x >= rect.x && x <= rect.x + rect.width
             && y >= rect.y && rect.y <= rect.y + rect.height) 
